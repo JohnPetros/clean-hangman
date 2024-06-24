afe143e9228570ec43aaaef8524574bf53b85682
JohnPetros <joaopedro.nc@outlook.com>
2024-06-24 10:27:34 -0300

    🖥️ views: add category prompt view

---
+++ packages/cli/views/category_prompt_view.py
@@ -0,0 +1,24 @@
+from cli.utils import prompt, console
+
+from core.constants import CATEGORIES_WORDS, CATEGORIES_EMOJIS
+
+
+def category_prompt_view():
+    categories = CATEGORIES_WORDS._fields
+    letters = [category[0] for category in categories]
+
+    letter = ""
+
+    while True:
+        letter = prompt.ask("[blue]Category letter[/blue]").strip().lower()
+
+        if letter not in letters:
+            console.print("[red b]Invalid letter 😡[/red b]")
+        else:
+            break
+
+    filtered_categories = filter(lambda category: category[0] == letter, categories)
+    category_name = list(filtered_categories)[0]
+    category_emoji = getattr(CATEGORIES_EMOJIS, category_name)
+
+    return category_name, category_emoji
