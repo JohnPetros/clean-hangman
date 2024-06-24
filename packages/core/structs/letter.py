ec66e90038221174d29eba26a153fb0134f348d7
JohnPetros <joaopedro.nc@outlook.com>
2024-06-24 10:32:58 -0300

    🧩 structs: further validate letter value

--- packages/core/structs/letter.py
+++ packages/core/structs/letter.py
@@ -1,10 +1,24 @@
 from dataclasses import dataclass
 
+from core.constants import VALID_LETTERS
+
 
 @dataclass(frozen=True)
 class Letter:
     value: str
 
     def __post_init__(self):
+        self.__validate_value()
+
+    def __validate_value(self):
         if not isinstance(self.value, str):
-            raise ValueError("Letter error should be a string")
+            raise ValueError("Letter value should be a string")
+
+        if not self.value.isalpha():
+            raise ValueError("Letter value should be a text")
+
+        if len(self.value) != 1:
+            raise ValueError("Letter value's length should equal to one")
+
+        if self.value not in VALID_LETTERS:
+            raise ValueError(f"{self.value} value should be a valid letter")
