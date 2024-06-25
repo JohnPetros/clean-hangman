32edccbf7e2f6972758c8165af38d0a1f74130b1
JohnPetros <joaopedro.nc@outlook.com>
2024-06-25 20:51:33 -0300

    🖥️ views: add home

---
+++ packages/gui/app/screens/home/home_view.py
@@ -0,0 +1,26 @@
+from flet import CrossAxisAlignment
+
+from gui.app.types import UI
+from gui.app.components import (
+    categories_table_dialog_view,
+)
+
+from .home_controller import HomeController
+
+
+def home_view(ui: UI):
+    ui.title = "Hangman - A John Petros' Game"
+    ui.bgcolor = "#34495E"
+    ui.horizontal_alignment = CrossAxisAlignment.CENTER
+    ui.window_height = 820
+
+    home_controller = HomeController(ui)
+
+    categories_table_dialog = categories_table_dialog_view(
+        on_choose=home_controller.handle_categories_dialog_choose
+    )
+
+    ui.overlay.append(categories_table_dialog)
+    categories_table_dialog.open = True
+
+    ui.update()
