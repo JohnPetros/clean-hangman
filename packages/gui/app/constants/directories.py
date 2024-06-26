bdfda1cb53913978200cb486cc8941099b5745cd
JohnPetros <joaopedro.nc@outlook.com>
2024-06-26 17:19:33 -0300

    🐛 fix: assets directory constant path

--- packages/gui/app/constants/directories.py
+++ packages/gui/app/constants/directories.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 directories_tuple = namedtuple("DIRECTORIES", ["assets"])
 
-path = Path()
+path = Path(__file__).parent.parent.parent.parent
 
 DIRECTORIES = directories_tuple(str(path.absolute() / "gui" / "assets"))
