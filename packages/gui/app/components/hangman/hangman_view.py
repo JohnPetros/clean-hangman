d46d9b02303e394145b5b6fcff754fd9d29e5629
JohnPetros <joaopedro.nc@outlook.com>
2024-06-28 15:37:01 -0300

    🐛 fix: set hangman limbs images base path

--- packages/gui/app/components/hangman/hangman_view.py
+++ packages/gui/app/components/hangman/hangman_view.py
@@ -1,14 +1,12 @@
 from flet import Column, Image, Row, Text, MainAxisAlignment, colors
 
-from gui.app.constants import DIRECTORIES
-
 HANGMAN_LIMBS = ["hang", "head", "right-hand", "left-hand", "right-leg", "left-leg"]
 
 
 def hangman_view(attempts_count: int):
     limb = HANGMAN_LIMBS[-attempts_count + 5]
 
-    image = Image(src=f"{DIRECTORIES.assets}/{limb}.svg", height=250)
+    image = Image(src=f"{limb}.svg", height=250)
 
     attempts_count_text = Row(
         [
