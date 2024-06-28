997699218d22478e7b31db4706fb8114df76943d
JohnPetros <joaopedro.nc@outlook.com>
2024-06-28 12:46:35 -0300

    ⚙️ config: set app view for gui

--- packages/gui/app/__init__.py
+++ packages/gui/app/__init__.py
@@ -1,4 +1,4 @@
-from flet import app
+from flet import app, AppView
 
 from gui.app.types import UI
 from gui.app.screens import init_screens
@@ -9,4 +9,4 @@ def init_app():
     def init_ui(ui: UI):
         init_screens(ui)
 
-    app(target=init_ui, assets_dir=DIRECTORIES.assets)
+    app(target=init_ui, assets_dir=DIRECTORIES.assets, view=AppView.WEB_BROWSER, port=46857)
